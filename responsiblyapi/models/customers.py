d0a017625aea8af3d80dfe5a8799f1d38aba8012
kimberly burton <kj.burton03@gmail.com>
2023-04-10 07:57:50 -0500

    first commit

---
+++ responsiblyapi/models/customers.py
@@ -0,0 +1,13 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Customers(models.Model):
+#     address=models.CharField(max_length=120)
+#     phoneNumber=models.CharField(max_length=120)
+#     # userId = models.ForeignKey("User", on_delete=models.CASCADE)
+
+
+    #   "id": 2,
+    #   "address": "56849 Fadel Gateway",
+    #   "phoneNumber": "202-244-7090",
+#   "userId": 1
